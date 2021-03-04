86451e43dc582d66d43c1779de2dc787c33b4b4b
apple <739945014@qq.com>
2021-03-04 12:49:34 +0800

    搜索页面可以进行滚动

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -4,10 +4,16 @@
 			<!-- 状态栏 -->
 			<view class="navbar_status" :style="{height: statusBarHeight+'px'}"></view>
 			<!-- 搜索框 -->
-			<view class="navbar_content" :style="{height: contentHeight+'px', width: searchWidth+'rpx'}">
-				<view class="navbar_content_search" >
+			<view class="navbar_content" :class="{search:hasBackIcon}" :style="{height: contentHeight+'px', width: searchWidth+'rpx'}">
+				<view v-if="hasBackIcon" class="navbar_back_icon" @click="toBack">
+					<uni-icons type="back" color="#fff" size="20"></uni-icons>
+				</view>
+				<view v-if="!hasBackIcon" class="navbar_content_search" @click="toSearchPage" >
 					<view class="navbar_content_search_icon iconfont icon-search"></view>
 					<view class="navbar_content_search_text">uni-app vue</view>
+				</view>
+				<view v-else class="navbar_content_search">
+					<input class="navbar_content_search_text" type="text" value="" placeholder="请输入您要搜索的内容" />
 				</view>
 			</view>
 		</view>
@@ -24,6 +30,12 @@
 				searchWidth: 720
 			};
 		},
+		props: {
+			hasBackIcon: {
+				type: Boolean,
+				default: false
+			}
+		},
 		created() {
 			//获取导航栏的高度
 			const info = uni.getSystemInfoSync()
@@ -35,6 +47,17 @@
 			this.contentHeight = (menuButtonInfo.bottom - this.statusBarHeight) + (menuButtonInfo.top - this.statusBarHeight)
 			this.searchWidth = 750 - menuButtonInfo.left
 			// #endif
+		},
+		methods: {
+			toSearchPage() {
+				console.log("跳转")
+				uni.navigateTo({
+					url: "/pages/home-search/home-search"
+				})
+			},
+			toBack() {
+				uni.navigateBack()
+			}
 		}
 	}
 </script>
@@ -48,6 +71,7 @@
 		background-color: $them-color;
 		width: 100%;
 		box-sizing: border-box;
+		z-index: 99;
 		.navbar_status {
 			
 		}
@@ -55,7 +79,9 @@
 			padding: 0 15rpx;
 			display: flex;
 			align-items: center;
+			
 			.navbar_content_search {
+				flex: 1;
 				width: 100%;
 				padding: 0 30rpx;
 				background-color: #ffffff;
@@ -69,6 +95,23 @@
 				.navbar_content_search_text {
 					font-size: 30rpx;
 					color: #999;
+				}
+			}
+		}
+
+		.search {
+			padding-left: 0;
+			.navbar_back_icon {
+				width: 60rpx;
+				height: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+			.navbar_content_search {
+				border-radius: 10rpx;
+				.navbar_content_search_text {
+					width: 100%;
 				}
 			}
 		}
---
+++ pages/home-search/home-search.vue
@@ -0,0 +1,91 @@
+<template>
+	<view class="search">
+		<navbar :hasBackIcon="true"></navbar>
+		<view class="search_history">
+			<view class="search_history_header">
+				<view>历史记录</view>
+				<view>清空</view>
+			</view>
+			<scroll-view class="search_history_content" scroll-y="true">
+				<view class="search_history_content_list">
+					<view class="search_history_content_item" v-for="item in 30">
+						{{item}}的内容
+					</view>
+				</view>
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+	page {
+		height: 100%;
+	}
+	
+	.search {
+		height: 100%;
+		background-color: #f5f5f5;
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		.search_history {
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			overflow: hidden;
+			.search_history_header {
+				background-color: #FFFFFF;
+				display: flex;
+				justify-content: space-between;
+				padding: 0 15px;
+				height: 50px;
+				line-height: 50px;
+				view:nth-child(1) {
+					color: #007AFF;
+				}
+				view:nth-child(2) {
+					color: $them-color;
+					font-weight: bold;
+				}
+			}
+			.search_history_content {
+				flex: 1;
+				box-sizing: border-box;
+				overflow: hidden;
+				.search_history_content_list {
+					background-color: #FFFFFF;
+					display: flex;
+					flex-wrap: wrap;
+					padding: 0px 15px 15px;
+					.search_history_content_item {
+						padding: 5px 10px;
+						font-size: 15px;
+						color: #333333;
+						border: 1px solid #333333;
+						text-align: center;
+						border-radius: 5px;
+						margin-right: 10px;
+						margin-bottom: 10px;
+					}
+				}
+					
+			}
+		}
+	}
+	
+	
+</style>
